bac537ebfc1b81b4267416c6e7564799a9a959d0
TENX <coopersjy@gmail.com>
2020-05-21 20:08:30 +0800

    commit

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,44 +1,41 @@
 #![allow(non_snake_case)]
 
+/// Code that exists only for debugging,
+/// if you want to see algorithms, please move to algorthms
 
-use rand::Rng;
 
-const USIZE_MAX:usize = std::usize::MAX;
+//const USIZE_MAX:usize = std::usize::MAX;
 
 fn main() {
-    use rand::prelude::*;
+    //use rand::{Rng, prelude::*;}
     // let mut L = vec![4, 5, 6, 7, 1, 2 ,3];
     // insertion_sort(&mut L);
     // println!("{:?}", L);
-
     // let mut rng = rand::thread_rng();
     // let mut nums: Vec<i32> = (1..100).collect();
     // nums.shuffle(&mut rng);
     // nums.sort();
     // println!("{:?}", nums);
-
-
-
-    println!("{:?}", nums_set);
+    // println!("{:?}", nums_set);
 
 
 }
 
 
-fn insertion_sort(arr: &mut Vec<i32>) {
-    for j in 1..arr.len() {
-        let key = arr[j];
-        let mut i = j-1;
-
-        while i != USIZE_MAX && arr[i] > key {
-            arr[i+1] = arr[i];
-            i = i.wrapping_sub(1);
-        }
-
-        if i == USIZE_MAX {
-            arr[0] = key
-        } else {
-            arr[i+1] = key
-        }
-    }
-}
+// fn insertion_sort(arr: &mut Vec<i32>) {
+//     for j in 1..arr.len() {
+//         let key = arr[j];
+//         let mut i = j-1;
+//
+//         while i != USIZE_MAX && arr[i] > key {
+//             arr[i+1] = arr[i];
+//             i = i.wrapping_sub(1);
+//         }
+//
+//         if i == USIZE_MAX {
+//             arr[0] = key
+//         } else {
+//             arr[i+1] = key
+//         }
+//     }
+// }
